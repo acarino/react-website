6b60916d464df4cdf87078be27e0a868e33e5849
Alan Carino <alan.carino@thomsonreuters.com>
2018-04-21 12:14:31 -0400

    added authorization from reltime db and integrated it with routes, nav, and admin page
    	public/wave.png

---
+++ src/firebase/firebasedb.jsx
@@ -0,0 +1,16 @@
+import { db } from './firebase';
+
+// User API
+
+export const doCreateUser = (id, username, email,admin) =>
+  db.ref(`users/${id}`).set({
+    username,
+    email,
+    admin,
+  });
+
+export const onceGetUsers = () =>
+  db.ref('users').once('value');
+
+export const GetUsersRole = (uid) =>
+    !!uid ?db.ref('users').child(uid).once('value') : '';
