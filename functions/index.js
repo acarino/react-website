c1015b7299234d19d6cbdb20c2c6f13b4d1c10e2
Alan Carino <alan.carino@thomsonreuters.com>
2018-04-27 10:43:54 -0400

    working with cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({origin: true});
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,11 +13,41 @@ const functions = require('firebase-functions');
   response.set('Access-Control-Allow-Origin', "*")
   response.set('Access-Control-Allow-Methods', 'GET, POST')
   response.set('Access-Control-Allow-Headers', "content-type")
+
   console.log("got a request and logging it")
+
   const requesttext = request.body.data;
+   if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+     console.log('Found Auth header');
+     const idToken = request.headers.authorization.split('Bearer ')[1];
+  }
+  else {
+    console.log('no auth header found');
+
+    idToken = null; //request.cookies.__session;
+  }
+  if(idToken !== null)
+  {
+    admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    request.user = decodedIdToken;
+    return next();
+    }).catch((error) => {
+      console.error('Error while verifying Firebase ID token:', error);
+    //request.status(403).send('Unauthorized');
+    });
+  }
+
+
+
+
+
   console.log("here is the text: "+requesttext)
-const responsData =  {"data":{"myData":"Hello finally from Firebase jason!"}}
+
+  const responsData =  {"data":{"myData":"Hello finally from Firebase jason!"}}
+
   response.send(responsData);
+
  });
 
  exports.helloWorld1 = functions.https.onRequest((request, response) => {
--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,14 +1,8 @@
 import * as firebase from 'firebase';
 import '@firebase/functions'
+import {config} from './firebasekey.jsx'
 
-var config = {
-  apiKey: "AIzaSyCDWJ8C32UTmCUEA5On9LAc6Q3Q-_UUnms",
-  authDomain: "crowdsurfer-2fccd.firebaseapp.com",
-  databaseURL: "https://crowdsurfer-2fccd.firebaseio.com",
-  projectId: "crowdsurfer-2fccd",
-  storageBucket: "crowdsurfer-2fccd.appspot.com",
-  messagingSenderId: "558716558956"
-};
+
 //initialize default firebase app for auth and db
 if(!firebase.apps[0] || firebase.apps[0].name !== "[DEFAULT]"){
   firebase.initializeApp(config);
